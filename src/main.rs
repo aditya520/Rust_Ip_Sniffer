adacfbbfd247c8237deb21c9bfdaedb9e57ce9cf
Aditya Arora <aditya.arora@Adityas-MacBook-Pro.local>
2020-01-13 16:36:57 +0530

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::io::{self, Write};
 use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
@@ -8,47 +9,68 @@ use std::thread;
 const MAX: u16 = 65535;
 
 struct Arguments {
-    flag: String,
-    ipaddr: IpAddr,
+    flag: String, 
+    ipaddr: IpAddr, 
     threads: u16,
 }
 
-impl Arguments{
+impl Arguments {
     fn new(args: &[String]) -> Result<Arguments, &'static str> {
         if args.len() < 2 {
-            return Err("not enough Arguments");
+            return Err("not enough arguments");
         } else if args.len() > 4 {
-            return Err("too mant arguments");
+            return Err("too many arguments");
         }
         let f = args[1].clone();
         if let Ok(ipaddr) = IpAddr::from_str(&f) {
-            return Ok(Arguments{flag: String::from(""), ipaddr, threads: 4})
+            return Ok(Arguments {flag: String::from(""), ipaddr, threads: 4});
         } else {
             let flag = args[1].clone();
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
-                println!("Usage: -j to select how many thready do you want
-                \r\n    -h or -help  to show this help message");
+                println!("Usage: -j to select how many threads you want
+                \n\r       -h or -help to show this help message");
                 return Err("help");
             } else if flag.contains("-h") || flag.contains("-help") {
                 return Err("too many arguments");
-            } else if flag.contains("-j"){
+            } else if flag.contains("-j") {
                 let ipaddr = match IpAddr::from_str(&args[3]) {
                     Ok(s) => s,
-                    Err(_) => return Err("not a valid IP Address, must be IPv4 or IPv6 ")
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6")
                 };
                 let threads = match args[2].parse::<u16>(){
                     Ok(s) => s,
-                    Err(_) => return Err("Failed to pass thread number"),
+                    Err(_) => return Err("failed to parse thread number")
                 };
-                return Ok(Arguments{threads,flag,ipaddr});
+                return Ok(Arguments{threads, flag, ipaddr});
             } else {
-                return Err("invalid Syntax");
+                return Err("invalid syntax");
             }
-        }
+        } 
+        
     }
 }
 
-fn main(){
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}    
+            
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
+fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     let arguments = Arguments::new(&args).unwrap_or_else(
@@ -56,20 +78,32 @@ fn main(){
             if err.contains("help") {
                 process::exit(0);
             } else {
-                eprintln!("{} problem parsring arguments: {}", program, err);
+                eprintln!("{} problem parsing arguments: {}", program, err);
                 process::exit(0);
             }
         }
     );
 
-    let number_threads = arguments.threads;
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
     let (tx, rx) = channel();
-    for i in 0..number_threads {
+    for i in 0..num_threads {
         let tx = tx.clone();
 
         thread::spawn(move || {
-            scan(tx, i, arguments.ipaddr, number_threads);
+            scan(tx, i, addr, num_threads);
         });
     }
 
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }    
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
